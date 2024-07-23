252661b444fb89ed07b7e7045485969b25df2a10
trifles <132377611+unconsideredtrifles@users.noreply.github.com>
2024-07-24 00:27:24 +0630

    Implement inserting node function to Tree

--- bst.js
+++ bst.js
@@ -3,4 +3,5 @@ import Tree from './tree-obj.js';
 
 const arr = [1, 2, 3, 4, 5, 6, 7];
 const tree = new Tree(arr);
+tree.insert(8);
 printTree(tree.root);
--- tree-obj.js
+++ tree-obj.js
@@ -1,4 +1,4 @@
-import { buildTree } from './tree-utils.js';
+import { Node, buildTree } from './tree-utils.js';
 
 class Tree {
   #root;
@@ -8,6 +8,27 @@ class Tree {
 
   get root() {
     return this.#root;
+  }
+
+  insert(value) {
+    if (this.#root === null) {
+      this.#root = new Node(value);
+      return;
+    }
+
+    let currentNode = this.#root;
+    let nodeDirection = (value < currentNode.value) ? 'left' : 'right';
+    while (currentNode[nodeDirection] !== null) {
+      currentNode = currentNode[nodeDirection];
+      if (value < currentNode.value) {
+        nodeDirection = 'left';
+      } else if (value > currentNode.value) {
+        nodeDirection = 'right';
+      } else {
+        throw new Error('no duplicate insertion pls');
+      }
+    }
+    currentNode[nodeDirection] = new Node(value);
   }
 }
 
