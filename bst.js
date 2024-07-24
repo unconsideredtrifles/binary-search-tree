12e4014d3d43bc8261f911552b23e14ee13ba550
trifles <132377611+unconsideredtrifles@users.noreply.github.com>
2024-07-24 22:56:59 +0630

    Implement deleting node in Tree

--- bst.js
+++ bst.js
@@ -5,3 +5,6 @@ const arr = [1, 2, 3, 4, 5, 6, 7];
 const tree = new Tree(arr);
 tree.insert(8);
 printTree(tree.root);
+console.log('Removing \'4\' from the tree');
+tree.delete(4);
+printTree(tree.root);
--- tree-obj.js
+++ tree-obj.js
@@ -30,6 +30,62 @@ class Tree {
     }
     currentNode[nodeDirection] = new Node(value);
   }
+
+  delete(value) {
+    // find the node to delete
+    // check if the node is left-nulled or right-nulled or both.
+    // If left-nulled, make its parent point to its right.
+    // If right-nulled, make its parent point to its left.
+
+    if (this.#root === null) {
+      return;
+    }
+
+    Tree.removeNode(this.#root, value);
+  }
+
+  static removeNode(startingNode, value, parent = null, traversedDirection = null) {
+    let currentNode = startingNode;
+    let parentNode = parent;
+    let lastTraversedDirection = traversedDirection;
+
+    while (currentNode !== null) {
+      if (value === currentNode.value) {
+        break;
+      }
+      parentNode = currentNode;
+      if (value < currentNode.value) {
+        currentNode = currentNode.left;
+        lastTraversedDirection = 'left';
+      } else if (value > currentNode.value) {
+        currentNode = currentNode.right;
+        lastTraversedDirection = 'right';
+      }
+    }
+
+    if (currentNode.left === null) {
+      parentNode[lastTraversedDirection] = currentNode.right;
+    } else if (currentNode.right === null) {
+      parentNode[lastTraversedDirection] = currentNode.left;
+    } else {
+      const inOrderSuccessor = Tree.getInOrderSuccessor(currentNode.right);
+      currentNode.value = inOrderSuccessor.value;
+      Tree.removeNode(
+        currentNode.right,
+        inOrderSuccessor.value,
+        currentNode,
+        'right',
+      );
+    }
+  }
+
+  static getInOrderSuccessor(node) {
+    let currentNode = node;
+    while (currentNode.left !== null) {
+      currentNode = currentNode.left;
+    }
+    return currentNode;
+  }
 }
 
 export default Tree;
